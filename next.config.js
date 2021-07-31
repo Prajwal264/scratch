1c1ac415331467b41ae6a38464d23b143e29c7d4
Prajwal264 <prajwalp264@gmail.com>
2021-07-31 18:07:31 +0530

    init

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  future: {
+    webpack5: false,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "@styles/_global.scss";
+import { AppProps } from "next/app";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
