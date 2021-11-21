5bfa0e6da99156f476b33709463838c8dfced813
Prajwal P <prajwalp264@gmail.com>
2021-11-21 20:26:38 +0530

    (feat): react-dropzone image upload front end logic added

--- components/profile/EditProfileCard.tsx
+++ components/profile/EditProfileCard.tsx
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsPencil } from "react-icons/bs";
 import { FiSettings, FiLogOut } from "react-icons/fi";
 
 import styles from "@styles/components/EditProfileCard.module.scss";
+import EditProfilePopup from "@components/popups/EditProfilePopup";
 
 /**
  *
  *
  * @return {*}
  */
-const EditProfileCard: React.FC = () => (
-  <div className={styles.editProfileCard}>
-    <ul>
-      <li>
-        <BsPencil />
-        <p>Edit Profile</p>
-      </li>
-      <li>
-        <FiSettings />
-        <p>Settings</p>
-      </li>
-      <li>
-        <FiLogOut />
-        <p>Logout</p>
-      </li>
-    </ul>
-  </div>
-);
+const EditProfileCard: React.FC = () => {
+  const [showEditProfilePopup, setShowEditProfilePopup] = useState(false);
+  return (
+    <div className={styles.editProfileCard}>
+      <ul>
+        <li>
+          <button
+            type="button"
+            onClick={() => setShowEditProfilePopup(!showEditProfilePopup)}
+          >
+            <BsPencil />
+            <p>Edit Profile</p>
+          </button>
+        </li>
+        <li>
+          <button type="button">
+            <FiSettings />
+            <p>Settings</p>
+          </button>
+        </li>
+        <li>
+          <button type="button">
+            <FiLogOut />
+            <p>Logout</p>
+          </button>
+        </li>
+      </ul>
+      <EditProfilePopup
+        showModal={showEditProfilePopup}
+        closeModal={() => setShowEditProfilePopup(false)}
+      />
+    </div>
+  );
+};
 
 export default EditProfileCard;
