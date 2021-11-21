a0fc078688056f7cd58091c3bf372a477f32cc74
Prajwal P <prajwalp264@gmail.com>
2021-11-21 12:47:53 +0530

    (feat): here comes apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,13 @@ import React from "react";
 import "@styles/_utils/fonts/index.scss";
 import { AppProps } from "next/app";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo/helpers/apollo-client";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
 );
 
 export default MyApp;
